838b9a3465ef32b58241fbfcb8c27735c2b0ce47
ynaka6 <yuuki.nakamura.0828@gmail.com>
2020-09-22 12:36:49 +0800

    :wrench: integrate jest test

---
+++ resources/js/components/__tests__/AppLogo.spec.js
@@ -0,0 +1,38 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import VueRouter from 'vue-router'
+import AppLogo from '../AppLogo'
+
+const localVue = createLocalVue()
+localVue.use(VueRouter)
+const router = new VueRouter()
+
+describe('AppLogo', () => {
+  it('props.tagNameにdivを指定した場合、ロゴのImalogが表示される', () => {
+    const wrapper = shallowMount(AppLogo,  {
+      propsData: {
+        tagName: 'div'
+      }
+    })
+    expect(wrapper.text()).toBe('Imalog')
+  })
+  it('props.tagNameにspanを指定した場合、ロゴのImalogが表示される', () => {
+    const wrapper = shallowMount(AppLogo,  {
+      propsData: {
+        tagName: 'span'
+      }
+    })
+    expect(wrapper.text()).toBe('Imalog')
+  })
+
+  it('props.tagNameにrouter-linkを指定した場合、clickするとトップページへ', () => {
+    const wrapper = shallowMount(AppLogo,  {
+      propsData: {
+        tagName: 'router-link',
+        to: '/'
+      },
+      localVue,
+      router
+    })
+    expect(wrapper.vm.$route.path).toBe('/')
+  })
+})
