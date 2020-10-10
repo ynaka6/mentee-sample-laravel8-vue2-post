83a2a80a6c8934c793906676424200a72068bd9d
ynaka6 <yuuki.nakamura.0828@gmail.com>
2020-10-10 11:45:36 +0800

    :sparkles: added cloudinary upload

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        CloudinaryLabs\CloudinaryLaravel\CloudinaryServiceProvider::class,
 
         /*
          * Application Service Providers...
