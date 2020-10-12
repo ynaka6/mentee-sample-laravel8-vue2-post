279995e4cd9e31ddb255de76a6c54856a1a83471
ynaka6 <ynaka6@users.noreply.github.com>
2020-10-12 05:43:43 +0000

    Apply php-cs-fixer changes

--- app/Models/PostProduct.php
+++ app/Models/PostProduct.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-
 class PostProduct extends Model
 {
     use HasFactory, SoftDeletes;
